6e9b065b8618c2010073d225667360c124de1eaa
Pawel Trybulski <pawel.trybulski@payair.com>
2023-10-11 12:30:24 +0200

    Initial Commit

---
+++ slideshowSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  slideshowSwiftUI
+//
+//  Created by Paweł Trybulski on 11/10/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ slideshowSwiftUI/slideshowSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  slideshowSwiftUIApp.swift
+//  slideshowSwiftUI
+//
+//  Created by Paweł Trybulski on 11/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct slideshowSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
