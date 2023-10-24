ea83270e8be448d367c638d34212cf5e962c646a
Pawel Trybulski <pawel.trybulski@payair.com>
2023-10-24 15:18:40 +0200

    Changed app name to "Slideshower for macos"

---
+++ slideshowSwiftUI/AboutView.swift
@@ -0,0 +1,18 @@
+//
+//  AboutView.swift
+//  Slideshower
+//
+//  Created by Paweł Trybulski on 24/10/2023.
+//
+
+import SwiftUI
+
+struct AboutView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AboutView()
+}
